0d60fc4ecfe02d9124d405a50fd1fd4551cebd67
Adrien Gallouët <adrien@gallouet.fr>
2025-12-03 19:58:32 +0100

    Simplify and fix windows
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- unzstd.c
+++ unzstd.c
@@ -2,53 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _WIN32
+#include <fcntl.h>
+#endif
+
 #include "zstddeclib.c"
 
 #define BUF_SIZE (16*1024)
 
-static void
-die(const char *msg)
-{
-    if (msg) {
-        size_t len = strlen(msg);
-        write(2, msg, len);
-    }
-    exit(1);
-}
-
-static void
-write_all(unsigned char *data, size_t size)
+static int
+write_all(const unsigned char *data, size_t size)
 {
     while (size > 0) {
-        ssize_t written = write(1, data, size);
+        ssize_t n = write(1, data, size);
 
-        if (written <= 0)
-            die("Couldn't write in stdout");
+        if (n <= 0)
+            return 1;
 
-        data += written;
-        size -= written;
+        data += n;
+        size -= n;
     }
+    return 0;
 }
 
 int
 main(void)
 {
+#ifdef _WIN32
+    _setmode(0, _O_BINARY);
+    _setmode(1, _O_BINARY);
+#endif
+
     ZSTD_DStream *dstream = ZSTD_createDStream();
 
     if (!dstream)
-        die("Couldn't init zstd");
+        return 1;
 
     size_t ret = ZSTD_initDStream(dstream);
 
     if (ZSTD_isError(ret))
-        die(ZSTD_getErrorName(ret));
+        return 2;
 
     _Alignas(16) unsigned char in[BUF_SIZE];
     _Alignas(16) unsigned char out[BUF_SIZE];
-    size_t read_size;
 
-    while ((read_size = read(0, in, BUF_SIZE)) > 0) {
-        ZSTD_inBuffer input = {in, read_size, 0};
+    for (;;) {
+        ssize_t r = read(0, in, BUF_SIZE);
+
+        if (!r)
+            break;
+
+        if (r <= 0)
+            return 3;
+
+        ZSTD_inBuffer input = {in, (size_t)r, 0};
 
         while (input.pos < input.size) {
             ZSTD_outBuffer output = {out, BUF_SIZE, 0};
@@ -56,14 +63,12 @@ main(void)
             size_t ret = ZSTD_decompressStream(dstream, &output, &input);
 
             if (ZSTD_isError(ret))
-                die(ZSTD_getErrorName(ret));
+                return 4;
 
-            write_all(out, output.pos);
+            if (write_all(out, output.pos))
+                return 5;
         }
     }
-    if (read_size < 0)
-        die("Couldn't read stdin");
-
     ZSTD_freeDStream(dstream);
     return 0;
 }
